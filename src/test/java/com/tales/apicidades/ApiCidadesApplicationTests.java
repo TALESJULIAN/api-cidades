d9d8af13f55256d503415772390442c4bf862ae8
TALESJULIAN <talesjulianl@gmail.com>
2019-07-18 11:40:09 -0300

    Initial Commit, changes in read.me and CI with Travis

---
+++ src/test/java/com/tales/apicidades/ApiCidadesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tales.apicidades;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCidadesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
